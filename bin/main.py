69dcfe6f70d915c795259d434dfed0b115090b96
Ted Sjöblom <36690474+TedSjoblom@users.noreply.github.com>
2024-05-30 21:05:44 +0200

    Update main.py

--- bin/main.py
+++ bin/main.py
@@ -242,7 +242,7 @@ def from_sensor(session: zenoh.Session, args: argparse.Namespace):
     apply_config.azimuth_window = (0, 36000)
     apply_config.lidar_mode = client.LidarMode.from_string("MODE_512x10")
     apply_config.operating_mode = client.OperatingMode.from_string("NORMAL")
-    client.set_config(args.ouster_hostname, config, persist=True)
+    client.set_config(args.ouster_hostname, apply_config, persist=True)
     
     
     logging.info("Connecting to Ouster sensor...")
